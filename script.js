67bd161d45272a6f9c63d2d7fbb2e4501c6455ad
Fela Pyne <opyne@wlapcs.org>
2017-02-15 23:45:03 +0000

    my first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+        function cleardisplay(){
+            
+      document.getElementById("display-bar").innerHTML =""; 
+                }
+        function display(){
+                document.getElementById("display-bar").innerHTML = "9"
+        }
